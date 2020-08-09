f0d0b69548a8b7be568c22d3203def2c4a99889d
nm-woodward <61477392+nm-woodward@users.noreply.github.com>
2020-08-09 17:04:26 -0400

    Delete applicationChart.js

--- applicationChart.js
+++
@@ -1,9 +0,0 @@
-/*
-d3.csv("STATEWIDE.csv").then(function(data) {
-  console.log(data[0]);
-});
-*/
-
-d3.csv("https://dl.dropboxusercontent.com/s/f8aju9n410fyql1/STATEWIDE.csv?dl=1").then(function(data) {
-  console.log(data[1]); // [{"Hello": "world"}, …]
-});
